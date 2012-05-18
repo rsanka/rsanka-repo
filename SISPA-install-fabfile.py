100ee74b1e47561bdb9c4f2afa7ca52db5296e83
Your Name <rsanka@jcvi.org>
2012-05-18 16:52:07 -0400

    fabfile now downloading and untar-ing REFS, RUBYLIB, and BINARIES tarballs

--- SISPA-install-fabfile.py
+++ SISPA-install-fabfile.py
@@ -9,37 +9,56 @@ from fabric.network import disconnect_all
 @task
 def install():
     try:
+        _define_dirs()
         _print_env_variables()
         _initialize_dirs()
         _add_tools()
+        _add_refs()
     finally:
         disconnect_all()
+
+def _define_dirs():
+    env.PROJECT_DIR = "%(ROOT_DIR)s/project" % env
+    env.REFS_DIR = "%(ROOT_DIR)s/refs" % env
+    env.TOOLS_DIR = "%(ROOT_DIR)s/tools" % env
+    env.TOOLS_RUBYLIB_DIR = "%(TOOLS_DIR)s/RUBYLIB" % env
+    env.TOOLS_BINARIES_DIR = "%(TOOLS_DIR)s/BINARIES" % env
 
 def _print_env_variables():
     print("user: %(user)s" % env)
     print("host: %(host)s" % env)
-    print("PROJECT ROOT: %(PROJECT_ROOT_DIR)s" % env)
-    print("SCRATCH ROOT: %(SCRATCH_ROOT_DIR)s" % env)
-    print("TOOLS ROOT: %(TOOLS_ROOT_DIR)s" % env)
+    print("ROOT: %s(ROOT_DIR)s" % env)
+    print("PROJECT: %(PROJECT_DIR)s" % env)
+    print("REFS: %(REFS_DIR)s" % env)
+    print("TOOLS: %(TOOLS_DIR)s" % env)
+    print("TOOLS RUBYLIB: %(TOOLS_RUBYLIB_DIR)s" % env)
+    print("TOOLS BINARIES: %(TOOLS_BINARIES_DIR)s" % env)
 
 def _initialize_dirs():
-    sudo("mkdir -p %(PROJECT_ROOT_DIR)s" % env)
-    sudo("mkdir -p %(SCRATCH_ROOT_DIR)s" % env)
-    sudo("mkdir -p %(TOOLS_ROOT_DIR)s" % env)
-    sudo("mkdir -p %(RUBYLIB_DIR)s" % env)
+    sudo("mkdir -p %(PROJECT_DIR)s" % env)
+    sudo("mkdir -p %(REFS_DIR)s" % env)
+    sudo("mkdir -p %(TOOLS_DIR)s" % env)
+    sudo("mkdir -p %(TOOLS_RUBYLIB_DIR)s" % env)
+    sudo("mkdir -p %(TOOLS_BINARIES_DIR)s" % env)
 
 def _add_tools():
-    _add_rubylib(env.RUBYLIB_URL,env.RUBYLIB_DIR)
+    _add_tarball(env.RUBYLIB_URL,env.RUBYLIB_TARBALL,env.TOOLS_RUBYLIB_DIR)
+    _add_tarball(env.BINARIES_URL,env.BINARIES_TARBALL,env.TOOLS_BINARIES_DIR)
 
-def _add_rubylib(download_url,install_dir):
-    with cd(install_dir):
-        print("du: %s" % download_url)
-        print("id: %s" % install_dir)
-        sudo("wget --no-check-certificate -O RUBYLIB.tar --directory-prefix=%s %s" % (install_dir,download_url))
-        sudo("tar xvf %s/RUBYLIB.tar -C %s" % (install_dir,install_dir))
+def _add_refs():
+    _add_tarball(env.REFS_URL,env.REFS_TARBALL,env.REFS_DIR)
+
+def _add_tarball(download_url,tarball,install_dir):
+    print("du: %s" % download_url)
+    print("tf: %s" % tarball)
+    print("id: %s" % install_dir)
+    sudo("wget --no-check-certificate -O %s %s" % (tarball,download_url))
+    sudo("mv %s %s" % (tarball,install_dir))
+    _untar(install_dir,tarball)
+
+def _untar(install_dir,tarball):
+    sudo("tar xvf %s/%s -C %s" % (install_dir,tarball,install_dir))
 
 @task
 def clean_up():
-    sudo("rm -fr %(PROJECT_ROOT_DIR)s" % env)
-    sudo("rm -fr %(SCRATCH_ROOT_DIR)s" % env)
-    sudo("rm -fr %(TOOLS_ROOT_DIR)s" % env)
+    sudo("rm -fr %(ROOT_DIR)s" % env)
