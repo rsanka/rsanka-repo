812b92c9903fc84d7f4ac238765184185eb6fc3b
Your Name <rsanka@jcvi.org>
2012-06-08 17:54:42 -0400

    Updating fab files

--- SISPA-install-fabfile.py
+++ SISPA-install-fabfile.py
@@ -6,6 +6,8 @@ import os.path, re
 from fabric.api import cd, env, hide, local, run, settings, sudo, task
 from fabric.network import disconnect_all
 
+directories = {}
+
 @task
 def install():
     try:
@@ -19,43 +21,34 @@ def install():
         disconnect_all()
 
 def _define_dirs():
-    env.PROJECT_DIR = "%(ROOT_DIR)s/project" % env
-    env.SAMPLE_DATA_DIR = "%(PROJECT_DIR)s/sample_data" % env
-    env.REF_FILES = "%(REF_FILES)s" % env
-    env.REF_DIR = "%(ROOT_DIR)s/refs" % env
-    env.TOOLS_DIR = "%(ROOT_DIR)s/tools" % env
-    env.TOOLS_RUBYLIB_DIR = "%(TOOLS_DIR)s/RUBYLIB" % env
-    env.TOOLS_BINARIES_DIR = "%(TOOLS_DIR)s/BINARIES" % env
+    directories["PROJECT_DIR"] = "%(ROOT_DIR)s/project" % env
+    directories["REF_DIR"] = "%(ROOT_DIR)s/references" % env
+    directories["TOOLS_DIR"] = "%(ROOT_DIR)s/tools" % env
+    directories["TOOLS_BINARIES_DIR"] = "%s/BINARIES" % directories["TOOLS_DIR"]
+    directories["TOOLS_PERL_DIR"] = "%s/PERL" % directories["TOOLS_DIR"]
+    directories["TOOLS_RUBYLIB_DIR"] = "%s/RUBYLIB" % directories["TOOLS_DIR"]
+    directories["TOOLS_SFF_DIR"] = "%s/SFF" % directories["TOOLS_DIR"]
 
 def _print_env_variables():
     print("user:                %(user)s" % env)
     print("host:                %(host)s" % env)
     print("ROOT DIR:            %(ROOT_DIR)s" % env)
-    print("PROJECT DIR:         %(PROJECT_DIR)s" % env)
-    print("SAMPLE DATA DIR:     %(SAMPLE_DATA_DIR)s" % env)
     print("REFS FILES:          %(REF_FILES)s" % env)
-    print("REFS DIR:            %(REF_DIR)s" % env)
-    print("TOOLS DIR:           %(TOOLS_DIR)s" % env)
-    print("TOOLS RUBYLIB DIR:   %(TOOLS_RUBYLIB_DIR)s" % env)
-    print("TOOLS BINARIES DIR:  %(TOOLS_BINARIES_DIR)s" % env)
 
 def _initialize_dirs():
-    sudo("mkdir -p %(PROJECT_DIR)s" % env)
-    sudo("mkdir -p %(SAMPLE_DATA_DIR)s" % env)
-    sudo("mkdir -p %(REF_DIR)s" % env)
-    sudo("mkdir -p %(TOOLS_DIR)s" % env)
-    sudo("mkdir -p %(TOOLS_RUBYLIB_DIR)s" % env)
-    sudo("mkdir -p %(TOOLS_BINARIES_DIR)s" % env)
+    for name in sorted(directories.keys()):
+        sudo("mkdir -p %s" % directories[name])
 
 def _add_tools():
-    _add_tarball(env.RUBYLIB_URL,env.RUBYLIB_TARBALL,env.TOOLS_RUBYLIB_DIR)
-    _add_tarball(env.BINARIES_URL,env.BINARIES_TARBALL,env.TOOLS_BINARIES_DIR)
+    _add_tarball(env.BINARIES_URL,env.BINARIES_TARBALL,directories["TOOLS_BINARIES_DIR"])
+    _add_tarball(env.PERL_URL,env.PERL_TARBALL,directories["TOOLS_PERL_DIR"])
+    _add_tarball(env.RUBYLIB_URL,env.RUBYLIB_TARBALL,directories["TOOLS_RUBYLIB_DIR"])
+    _add_tarball(env.SFF_URL,env.SFF_TARBALL,directories["TOOLS_SFF_DIR"])
 
 def _add_refs():
     files = (env.REF_FILES).split(",")
     for file in files:
-        sudo("mkdir -p %s/%s" % (env.SAMPLE_DATA_DIR,file))
-        _add_tarball("%s/%s.tgz" % (env.REF_URL,file),file,env.REF_DIR)
+        _add_tarball("%s/%s.tgz" % (env.REF_URL,file),file,directories["REF_DIR"])
 
 def _chmod(path):
     sudo("chmod -R 755 %s" % path)
